efc575c71dd0e252417cdf287169e08b149d3feb
jvmMachado <joaovmmachado@gmail.com>
2020-02-27 14:58:18 -0300

    Define layout and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
